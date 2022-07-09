e71e314c8c206cbde53e0f359bba98a1b3d842f3
rbidon <roussebidon@gmail.com>
2022-07-09 12:58:17 -0400

    Change the src for images in js

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ class Game {
         sleepiness.style.color = "green";
         boredom.innerHTML =this.boredom;
         boredom.style.color = "green";
-        document.querySelector(".brownEgg").src="../images/CropBrown.jpg"
+        document.querySelector(".brownEgg").src="./images/CropBrown.jpg"
     }
 }
 // Instantiate your Tamagotchi (If you created a class)
@@ -220,15 +220,15 @@ const ageUpTamagotchi = () => {
     if(tamagotchi.age === 5){
                 console.log("Cute Happy Chicken Eggs")
                 alert(`${tamagotachiName} has evolved`);
-                document.querySelector(".brownEgg").src ="../images/eggSmile.jpg"
+                document.querySelector(".brownEgg").src ="./images/eggSmile.jpg"
             } else if(tamagotchi.age === 9){
                 console.log("Little Chick")
                 alert(`${tamagotachiName} has evolved`);
-                document.querySelector(".brownEgg").src ="../images/littleChick.png"
+                document.querySelector(".brownEgg").src ="./images/littleChick.png"
             } else if(tamagotchi.age === 13){
                 console.log("Chicken Hen")
                 alert(`${tamagotachiName} has evolved`);
-                document.querySelector(".brownEgg").src ="../images/grownHen.jpg";
+                document.querySelector(".brownEgg").src ="./images/grownHen.jpg";
                }  else if(tamagotchi.age ===19){ 
                 clearInterval(agingUpTamagotchi);
                 clearInterval(increasingScaleTamagotchi)
